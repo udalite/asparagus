854fed3fd951403eb2edb85c4ce2b3b0cfe1c9dd
Nikita K <kislitsin@group-ib.ru>
2017-04-06 21:20:06 +0300

    w1 type for Relay

--- relay.py
+++ relay.py
@@ -2,26 +2,67 @@ import RPi.GPIO as GPIO
 from datetime import datetime
 from datetime import timedelta
 import logging
-
+import subprocess
+from subprocess import Popen
 
 class Relay:
-    def __init__(self, port_type, port_number):
-        self.port = port_number
-        self.port_type = port_type
-        if port_type == 'gpio':
+    def __init__(self, **relay_settings):
+        port_type = relay_settings['type']
+        if port_type == "gpio":
             if GPIO.getmode() is None:
                 GPIO.setmode(GPIO.BCM)
 #           current_mode = GPIO.gpio_function(port_number)
 #           if current_mode != GPIO.OUT:
-            GPIO.setup(port_number, GPIO.OUT)
+            self.port_type = port_type
+            self.port_number = relay_settings['port_number']
+            GPIO.setup(self.port_number, GPIO.OUT)
+        elif port_type == "w1":
+            self.port_type = port_type
+            self.w1_id = relay_settings["w1_id"]
+            self.port_number = relay_settings['port_number']
+            self.w1_path = "/sys/bus/w1/devices/%s/output" % self.w1_id
         else:
-            raise NameError('Unsupported port_type %s' % port_type)
+            raise NameError('Unsupported port_type %s' % self.port_type)
 
-    def switch_status(self, status):
-        GPIO.output(self.port, status)
+    def w1_read_status_int(self):
+        if self.port_type == "w1":
+            cmd = "dd if=%s bs=1 count=1 | hexdump" % self.w1_path
+            process = Popen(cmd, shell=True, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+            output = process.stdout.readline().strip()
+            status_hex_str = output[-2:]
+            if len(status_hex_str) == 0:
+                return False
+            else:
+                status_int = int(status_hex_str, 16)
+                return status_int
+
+    def w1_write_status_int(self, new_status_int):
+        if self.port_type == "w1":
+            new_status_hex = hex(new_status_int)[1:]
+            cmd = "echo -e \\\\%s | dd of=%s bs=1 count=1"\
+                  % (new_status_hex, self.w1_path)
+            Popen(cmd, shell=True, executable='/bin/bash',
+                  stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    def set_status(self, status):
+        if self.port_type == "gpio":
+            GPIO.output(self.port, status)
+        elif self.port_type == "w1":
+            status_int = self.w1_read_status_int()
+            if status == 1:
+                new_status_int = status_int & ~(1 << self.port)
+            elif status == 0:
+                new_status_int = status_int | 1 << self.port
+            self.w1_write_status_int(new_status_int)
 
     def get_status(self):
-        status = GPIO.input(self.port)
+        if self.port_type == "gpio":
+            status = GPIO.input(self.port)
+        elif self.port_type == "w1":
+            status_int = self.read_status_int()
+            bit_mask = 1 << self.port
+            status = (status_int & bit_mask == 0)
         return status
 
 
@@ -29,7 +70,7 @@ class RelaySet:
     def __init__(self, settings):
         self.relay_list = []
         for relay in settings['relays']:
-            relay['object'] = Relay(relay['type'], relay['port'])
+            relay['object'] = Relay(relay)
             self.relay_list.append(relay)
 
     @staticmethod
@@ -67,4 +108,4 @@ class RelaySet:
                 msg = "Switching to %s" % scheduled_status
                 print(msg)
                 logging.info(msg)
-                relay['object'].switch_status(scheduled_status)
+                relay['object'].set_status(scheduled_status)
